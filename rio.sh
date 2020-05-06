d2adc348ad6454a621422e4c71f3cde6efa60aa7
RioGantengOk <64851507+RioGantengOk@users.noreply.github.com>
2020-05-06 17:51:27 +0800

    Add files via upload

---
+++ rio.sh
@@ -0,0 +1,34 @@
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay By Rio
+echo $green"Selamat Datang Bos"
+echo
+sleep 2
+echo $blue"SilahKan Pilih Tools Menarik"
+echo
+echo $red"1.) Stabilkan Jaringan"
+echo
+echo $cyan"0.) Exit !!!"
+echo
+echo $white
+read -p "PilihMana : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet rionata | lolcat
+echo $yellow"Stabilkan Jaringan By Rio"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $yellow
+figlet "Bye"
+echo $white"Info Lebih Lanjut whatsapp:08997887727"
+sleep 3
+exit
+fi
